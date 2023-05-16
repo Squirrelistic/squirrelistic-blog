6bea8d40ca6ac4655f72763330e1aeae4ddf1676
Andrzej Jakuszyk <ajakuszyk@gmail.com>
2023-05-16 18:42:13 +0100

    How to Convert C# string to GUID added

---
+++ how_to_convert_csharp_string_to_guid/Program.cs
@@ -0,0 +1,4 @@
+﻿using how_to_convert_csharp_string_to_guid;
+
+Console.WriteLine("Hash the string and convert into Guid".ToGuid());
+Console.WriteLine("🐿🐇".ToGuid());
---
+++ how_to_convert_csharp_string_to_guid/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿namespace how_to_convert_csharp_string_to_guid
+{
+    public static class StringExtensions
+    {
+        private const ulong KnuthInitValue = 3074457345618258791ul;
+        private const ulong KnuthMultiValue = 3074457345618258799ul;
+
+        // Based on https://stackoverflow.com/questions/9545619/a-fast-hash-function-for-string-in-c-sharp
+        public static Guid ToGuid(this string str)
+        {
+            // Double Knuth hash
+            ulong hashedValueEven = KnuthInitValue;
+            ulong hashedValueOdd = KnuthInitValue;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    hashedValueEven += str[i];
+                    hashedValueEven *= KnuthMultiValue;
+                }
+                else
+                {
+                    hashedValueOdd += str[i];
+                    hashedValueOdd *= KnuthMultiValue;
+                }
+            }
+            return new Guid(new byte[] {
+                (byte)(0xff & hashedValueEven),
+                (byte)(0xff & hashedValueEven >> 8),
+                (byte)(0xff & hashedValueEven >> 16),
+                (byte)(0xff & hashedValueEven >> 24),
+                (byte)(0xff & hashedValueEven >> 32),
+                (byte)(0xff & hashedValueEven >> 40),
+                (byte)(0xff & hashedValueEven >> 48),
+                (byte)(0xff & hashedValueEven >> 56),
+                (byte)(0xff & hashedValueOdd),
+                (byte)(0xff & hashedValueOdd >> 8),
+                (byte)(0xff & hashedValueOdd >> 16),
+                (byte)(0xff & hashedValueOdd >> 24),
+                (byte)(0xff & hashedValueOdd >> 32),
+                (byte)(0xff & hashedValueOdd >> 40),
+                (byte)(0xff & hashedValueOdd >> 48),
+                (byte)(0xff & hashedValueOdd >> 56),
+            });
+        }
+    }
+}
